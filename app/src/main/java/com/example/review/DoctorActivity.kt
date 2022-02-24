023f3030049ab671d9aeb2ac9e82af64ebda0102
MaryamTaheri <99006728+maryamtaheri88@users.noreply.github.com>
2022-02-24 11:33:05 +0330

    change icon app

--- app/src/main/java/com/example/review/DoctorActivity.kt
+++ app/src/main/java/com/example/review/DoctorActivity.kt
@@ -2,10 +2,17 @@ package com.example.review
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.review.databinding.ActivityDoctorBinding
 
 class DoctorActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityDoctorBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_doctor)
+        binding = ActivityDoctorBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.textView1.text = getString(R.string.hello_world)
     }
+
 }
